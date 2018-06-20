a43c6128a070d1b651c05a31a318ca45e0cf0772
Nicole Houston <nicole.houston@cox.net>
2018-06-20 09:26:49 -0700

    Set up folders and files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Royal Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Hamburger", false);
